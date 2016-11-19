5fa73b2c91ab5f5691b87b97446ad46cc68b46b1
hxu <to.helen.xu@gmail.com>
2016-11-19 17:28:32 -0500

    python for evaluation and prediction

--- model_release/predict_synthetic.py
+++ model_release/predict_synthetic.py
@@ -1,6 +1,3 @@
-# coding: utf-8
-# In[2]:
-
 from os.path import join, dirname
 import sys
 #manual path definition
@@ -10,23 +7,29 @@ import cv2
 import numpy as np
 import caffe
 import os
+# walk through synthetic images with shadows and remove shadows
+# results go into directory with the same filename
+
 import direct_intrinsics as di
 
 project_path = '/afs/csail.mit.edu/u/y/ylkuo/project/cv_final/direct-intrinsics/'
 #project_path = '/home/hxu/di-final/'
+
 # not sure these paths are right
 # point to generated data with shadows
 input_path = 'data/synthetic/images/shadow/'
+
+#for cvpr
+#input_path = 'data/cvpr11/shadow'
+
 shadow_path = 'shadow/'
 result_path = 'results/'
 
-
 # Load net
 #modelfile = 'mit_final_barron_train.caffemodel'
+# TODO: check paths here
 modelfile = project_path + 'training/snapshot__iter_5000.caffemodel'
 net = caffe.Net('direct_intrinsics_without_shading.prototxt', modelfile, caffe.TEST)
-
-# In[4]:
 
 # get list of file names and paths to them in the input
 path = project_path + input_path
@@ -50,14 +53,11 @@ for (dirpath, dirnames, filenames) in os.walk(path):
         if not os.path.isdir(dir_o):
             os.makedirs(dir_o)
 
-        #manual definition
-        #definition of image we want to do prediction on
+        # read in image
         img = cv2.imread(imgpath)
 
-        # Predict by Direct intrinsics CNN
+        # Predict
         result = di.predict(net, img)
-
-        # In[6]:
 
         # Display results
         '''
