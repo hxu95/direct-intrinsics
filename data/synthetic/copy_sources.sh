784745dd1a8cd25aeb55df2162dc717de0364c17
hxu95 <to.helen.xu@gmail.com>
2016-12-04 17:30:07 -0500

    new training / test splits with updated room and street, removed sun-temple-inside

--- data/synthetic/copy_sources.sh
+++ data/synthetic/copy_sources.sh
@@ -2,10 +2,18 @@
 
 cd sources
 
+# 90 / 10 split
 cp source_shadow_full.txt source_shadow_test.txt
 cp source_shadow_full.txt source_shadow_train.txt
 
 cp source_noshadow_full.txt source_noshadow_test.txt
 cp source_noshadow_full.txt source_noshadow_train.txt
+
+# scene split
+cp source_shadow_full.txt source_shadow_test_2.txt
+cp source_shadow_full.txt source_shadow_train_2.txt
+
+cp source_noshadow_full.txt source_noshadow_test_2.txt
+cp source_noshadow_full.txt source_noshadow_train_2.txt
 
 #do the rest manually
