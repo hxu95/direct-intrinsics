58d5043a78a0ddf55ae43d65a03433f0775747cb
hxu95 <to.helen.xu@gmail.com>
2016-12-05 00:37:29 -0500

    fixing result generation framework

--- evaluation/synthetic_eval.py
+++ evaluation/synthetic_eval.py
@@ -6,10 +6,11 @@ from os.path import join, dirname
 
 from util import rmse
 
-import sys
+import sys, cv2
 import numpy as np
 import os
 
+# path to direct intrinsics folder 
 # project_path = '/afs/csail.mit.edu/u/y/ylkuo/project/cv_final/direct-intrinsics/'
 project_path = '/home/hxu/di-final/'
 
@@ -21,7 +22,11 @@ noshadow_path = 'data/synthetic/images/noshadow/'
 
 # path to folders for results and ground truth
 # in the synthetic case they are 1:1
+
+# path to experimental results
 experiment_path = project_path + result_path
+
+# path to ground truth
 truth_path = project_path + noshadow_path
 
 print experiment_path
@@ -76,7 +81,6 @@ for (dirpath, dirnames, filenames) in os.walk(experiment_path):
 '''
 print experiment_dict
 
-
 # check that they are the same size
 assert len(experiment_dict) == len(truth_dict)
 
@@ -90,6 +94,8 @@ for key in truth_dict:
     exp_file = experiment_dict[key]
     print 'truth: {}'.format(gt_file)
     print 'experiment: {}'.format(exp_file)
+
+
 '''
     # get paths of images
 
--- model_release/predict_synthetic.py
+++ model_release/predict_synthetic.py
@@ -5,59 +5,76 @@ import sys
 import matplotlib.pyplot as plt
 import cv2
 import numpy as np
-import caffe
+# TODO: 
+# import caffe
 import os
+
 # walk through synthetic images with shadows and remove shadows
-# results go into directory with the same filename
+# results go into directory data/results with the same filename
 
 import direct_intrinsics as di
 
-project_path = '/afs/csail.mit.edu/u/y/ylkuo/project/cv_final/direct-intrinsics/'
+# project_path = '/afs/csail.mit.edu/u/y/ylkuo/project/cv_final/direct-intrinsics/'
 #project_path = '/home/hxu/di-final/'
-
-# not sure these paths are right
+project_path = '/home/hxu/6.869/direct-intrinsics-final-project/'
 # point to generated data with shadows
-input_path = 'data/synthetic/images/shadow/'
-
-#for cvpr
-#input_path = 'data/cvpr11/shadow'
+img_path = project_path + 'data/synthetic/images/'
 
 shadow_path = 'shadow/'
 result_path = 'results/'
 
+input_path = img_path + shadow_path
+
+path_r = img_path + result_path
+print 'result path: ' + path_r
+#for cvpr
+#input_path = 'data/cvpr11/shadow'
+print os.path.exists(path_r)
+if not os.path.exists(path_r):
+    os.makedirs(path_r)
+else:
+    print "EXISTS " + path_r
+
 # Load net
-#modelfile = 'mit_final_barron_train.caffemodel'
-# TODO: check paths here
+# modelfile = 'mit_final_barron_train.caffemodel'
+# TODO: point to the prototxt and model that we want to use
 modelfile = project_path + 'training/snapshot__iter_5000.caffemodel'
-net = caffe.Net('direct_intrinsics_without_shading.prototxt', modelfile, caffe.TEST)
+proto = 'direct_intrinsics_without_shading.prototxt'
+
+# TODO: load the net
+# net = caffe.Net(proto, modelfile, caffe.TEST)
 
 # get list of file names and paths to them in the input
-path = project_path + input_path
-print path
+#path = project_path + input_path
+print 'input path: ' + input_path
 list_of_files = {}
 
 #walk through shadows, get all images and subdirectories
-for (dirpath, dirnames, filenames) in os.walk(path):
+for (dirpath, dirnames, filenames) in os.walk(input_path):
     for filename in filenames:
         #list_of_files[filename] = os.sep.join([dirpath, filename])
-        print filename
         imgpath = os.sep.join([dirpath, filename])
-        print imgpath
+        # print imgpath
 
         path_o = imgpath.replace(shadow_path, result_path)
 
         #path_o = join(input_path, result_path, paths[sc][i].replace('clean/', ''))
         dir_o = dirname(path_o)
-        print path_o
-        print dir_o
-        if not os.path.isdir(dir_o):
+
+
+        # dir_o = join(dir_out, basename(dir_scene))
+        try: 
             os.makedirs(dir_o)
+            print 'output path: ' + path_o
+            print 'output dirname: ' + dir_o
+        except:
+            pass
 
         # read in image
-        img = cv2.imread(imgpath)
+        # img = cv2.imread(imgpath)
 
         # Predict
-        result = di.predict(net, img)
+        # result = di.predict(net, img)
 
         # Display results
         '''
@@ -72,4 +89,4 @@ for (dirpath, dirnames, filenames) in os.walk(path):
         '''
         
         # save the result
-        imsave(path_o, result)
+        # imsave(path_o, result)
