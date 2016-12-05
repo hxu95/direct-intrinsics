6bb469399642f617ea596e107d1eff777b694683
hxu <to.helen.xu@gmail.com>
2016-12-05 13:29:25 -0500

    updated creating shadow mask in LAB also sharpening

--- data/synthetic/create_shadow_mask.py
+++ data/synthetic/create_shadow_mask.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 from os.path import join, basename, dirname
 import matplotlib.image as mpimg
 import numpy as np
-from scipy.misc import imread, imsave
+# from scipy.misc import imread, imsave
+import cv2
 
 import argparse
 parser = argparse.ArgumentParser()
@@ -22,40 +23,44 @@ prefix = './images/shadow'
 for dir_scene in sorted(glob(join(dir_shadow, '*'))):
     for path_shadow in sorted(glob(join(dir_scene, '*.png'))):
 
-
 #image2 = mpimg.imread(noshadow_path + path)
 #image3 = image1 - image2
 
-        shadow = mpimg.imread(path_shadow)
+        shadow = cv2.imread(path_shadow)
+        shadow = cv2.cvtColor(shadow, cv2.COLOR_BGR2LAB)
+        shadow_l_channel, _, _ = cv2.split(shadow)
+        # print shadow
         # mask = np.repeat((albedo.mean(2) != 0).astype(np.uint8)[..., np.newaxis] * 255, 3, 2)
 
         #f = os.path.join(path, filename)
         f = None
         if path_shadow.startswith(dir_shadow):
             f = path_shadow[len(dir_shadow) + 1:]
-            # print "f: " + f
-            # a.write(f[len(prefix):] + os.linesep) 
+
         path_noshadow = join(dir_noshadow, f)
         # print path_noshadow
         #path_noshadow = join(path_noshadow, basename(path_shadow))
-        noshadow = mpimg.imread(path_noshadow)
+        noshadow = cv2.imread(path_noshadow)
+        noshadow = cv2.cvtColor(noshadow, cv2.COLOR_BGR2LAB)
+        noshadow_l_channel, _, _ = cv2.split(noshadow)
 
         dir_o = join(dir_out, basename(dir_scene))
         try: os.makedirs(dir_o)
         except: pass
 
-        result = shadow - noshadow
-        #imshow(shadow - result)
-        
-        #imshow (noshadow)
-
-        recover = shadow - result
-
+        result = noshadow_l_channel - shadow_l_channel
+        # result = noshadow - shadow
+        #result = cv2.cvtColor(result, cv2.COLOR_BGR2LAB)
+        #result_l, _, _ = cv2.split(result)
+        # cv2.imsave('shadow', shadow_l_channel)
+        # cv2.imsave('noshadow', noshadow_l_channel)
         ''' 
-        for some reason the second one doesn't pass?
+        recover = shadow - result
+        
+        #for some reason the second one doesn't pass?
         try:
             assert recover.shape == noshadow.shape
-            assert(recover == noshadow).all()
+            assert(shadow == noshadow + result).all()
         except AssertionError:
             _, _, tb = sys.exc_info()
             traceback.print_tb(tb) # Fixed format
@@ -64,9 +69,25 @@ for dir_scene in sorted(glob(join(dir_shadow, '*'))):
 
             print('An error occurred on line {} in statement {}'.format(line, text))
             exit(1)
-        ''' 
+        '''
+        result[result > 230] = 0
+        # print noshadow_l_channel
+        # print shadow_l_channel
+        # print result
+        print 'path_shadow: ' + path_shadow
+        print 'path_noshadow: ' + path_noshadow
+        path_result = join(dir_o, basename(path_shadow))
+        print 'path_result ' + path_result
+    
+        # result = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
+        # result = (255-gray_image)
+
         # assert np.array_equal(np.array(noshadow), np.array(shadow - result))
-        imsave(join(dir_o, basename(path_shadow)), result)
+        cv2.imwrite(path_result, result)
+
+
+        #imsave(path_result, result_l)
+        #break
 
         '''
         plt.figure()
@@ -75,3 +96,4 @@ for dir_scene in sorted(glob(join(dir_shadow, '*'))):
         plt.show()
         '''
 
+    # break
--- evaluation/sharpen.py
+++ evaluation/sharpen.py
@@ -11,17 +11,10 @@
 
 import cv2
 import numpy as np
-
-
-#Linux window/threading setup code.
-cv2.startWindowThread()
-cv2.namedWindow("Original")
-cv2.namedWindow("Sharpen")
+import matplotlib.pyplot as plt
 
 #Load source / input image as grayscale, also works on color images...
 imgIn = cv2.imread("noshadow_mug_more_data_1000.png", cv2.IMREAD_COLOR)
-cv2.imshow("Original", imgIn)
-
 
 #Create the identity filter, but with the 1 shifted to the right!
 kernel = np.zeros( (9,9), np.float32)
@@ -39,7 +32,12 @@ kernel = kernel - boxFilter
 # very bright or very dark pixel surrounded by the opposite type.
 
 custom = cv2.filter2D(imgIn, -1, kernel)
-cv2.imshow("Sharpen", custom)
-
-
-cv2.waitKey(0)
+# cv2.imwrite("Sharpen", custom)
+custom = cv2.cvtColor(custom, cv2.COLOR_BGR2RGB)
+plt.figure()
+plt.title('sharpened')
+plt.imshow(custom)
+#
+#plt.title('shading')
+#plt.imshow(mask * s)
+plt.show()
